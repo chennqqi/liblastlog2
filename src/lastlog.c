22e64928ccdf8d1bed2fd839c91591b27d3b74c3
Robin Hack <rhack@redhat.com>
2014-04-13 21:37:46 +0200

    Devel commit.
    Lot of changes:
    
        * added new api
        * error handling

--- src/lastlog.c
+++ src/lastlog.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
+#include <sys/uio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <assert.h>
+#include <string.h>
 
 #include <lastlog.h>
+
+#include <stdio.h>
 
 #define QUOTE(name) #name
 #define STR(macro) QUOTE(macro)
@@ -20,17 +24,47 @@
 #define LASTLOG_PATH_LEN_PLUS (LASTLOG_PATH + 1)
 #define LASTLOG_FILE_LEN_PLUS (LASTLOG_FILE_LEN + 1)
 
-struct lastlog2 {
-    struct lastlog old_ll;
-    int new_items;
+struct ll_extension {
+    unsigned int extension_id;
+    unsigned long fail_logs;
+    char reserved[512];
 };
+
+/* TODO: move to header file. Add attributes. */
+static int ll_add_impl (const uid_t uid, const struct lastlog *const ll, const struct ll_extension *const ll_ex);
+static int ll_read_impl (const uid_t uid, struct lastlog *const ll, struct ll_extension *const ll_ex);
+
+#define EXTENSION_MAGIC 1
 
 static inline uid_t get_uid_dir (uid_t uid)
 {
     return (uid - (uid % 1000));
+};
+
+static int check_extension(unsigned int extension_id)
+{
+    switch (extension_id) {
+        case EXTENSION_MAGIC:
+            return 1;
+        default:
+            return 0;
+    }
+
+    abort();
 }
 
-int ll_read (const uid_t uid, struct lastlog2 *const ll)
+
+inline int ll_read (const uid_t uid, struct lastlog *const ll)
+{
+    return ll_read_impl (uid, ll, NULL);
+}
+
+inline int ll_read_ex (const uid_t uid, struct lastlog *const ll, struct ll_extension *const ll_ex)
+{
+    return ll_read_impl (uid, ll, ll_ex);
+}
+
+static int ll_read_impl (const uid_t uid, struct lastlog *const ll, struct ll_extension *const ll_ex)
 {
     assert (ll != NULL);
 
@@ -41,31 +75,76 @@ int ll_read (const uid_t uid, struct lastlog2 *const ll)
     const int ll_fd = open (path, O_RDONLY | O_CLOEXEC | O_NOFOLLOW);
     if (ll_fd == -1) {
         perror ("no log file");
-    }
-
-    struct stat st = {0};
-    if (fstat (ll_fd, &st) == -1)
-    {
-        perror ("stat");
-    }
-
-    /* check record size */
-    if (st.st_size != sizeof (*ll)) {
-        close (ll_fd);
         return -1;
     }
 
-    if (read (ll_fd, ll, sizeof(*ll)) == -1) {
+    struct stat st = {0};
+    if ((fstat (ll_fd, &st) == -1) 
+        || (st.st_size < (off_t)sizeof (*ll)))
+    {
         close (ll_fd);
-        perror ("read record");
+        perror ("stat");
+        return -1;
     }
 
+    /* Plain old format. */
+    if (st.st_size == sizeof(*ll)) {
+        memset (ll, 0, sizeof(*ll));
+        const ssize_t n = read (ll_fd, ll, sizeof(*ll));
+        if ((n == -1) || (n != sizeof(*ll))) {
+            close (ll_fd);
+            perror ("read record");
+            return -1;
+        }
+
+        close (ll_fd);
+        return 1;
+    }
+
+    /* Don't care about extensions. But size if bigger than expected... */
+    if (ll_ex == NULL) {
+        return -1;
+    }
+
+    /* Format with extensions */
+    if (st.st_size < (off_t)(sizeof (*ll) + sizeof (*ll_ex))) {
+        return -1;
+    }
+
+    struct iovec iov[2] = {
+        { .iov_base = ll, .iov_len = sizeof (*ll) },
+        { .iov_base = ll_ex, .iov_len = sizeof (*ll_ex) }
+    };
+
+    memset (ll, 0, sizeof (*ll));
+    memset (ll_ex, 0, sizeof (*ll_ex));
+    const ssize_t n = readv (ll_fd, iov, (sizeof (iov) / sizeof (iov[0])));
+    if ((n == -1) || (n != (sizeof (*ll) + sizeof (*ll_ex)))) {
+        close (ll_fd);
+        perror ("read record");
+        return -1;
+    }
     close (ll_fd);
+
+    if (!check_extension (ll_ex->extension_id))
+    {
+        return -1;
+    }
 
     return 1;
 }
 
-int ll_add (const uid_t uid, const struct lastlog2 *const ll)
+inline int ll_add (const uid_t uid, const struct lastlog *const ll)
+{
+    return ll_add_impl (uid, ll, NULL);
+}
+
+inline int ll_add_ex (const uid_t uid, const struct lastlog *const ll, const struct ll_extension *const ll_ex)
+{
+    return ll_add_impl (uid, ll, ll_ex);
+}
+
+static int ll_add_impl (const uid_t uid, const struct lastlog *const ll, const struct ll_extension *const ll_ex)
 {
     assert (ll != NULL);
 
@@ -73,12 +152,13 @@ int ll_add (const uid_t uid, const struct lastlog2 *const ll)
     /* We use one \0 char for \ char in sprintf. */
     char ll_path[LASTLOG_PATH_LEN] = {0};
     /* So... we have 3 NUL chars for free.. one we use for /. Second we use as normal and... */
-    char true_path[sizeof("/proc/self/fd/") + sizeof(STR (INT_MAX)) + sizeof(STR (UID_MAX)) - 1];
+    char true_path[sizeof ("/proc/self/fd/") + sizeof (STR (INT_MAX)) + sizeof (STR (UID_MAX)) - 1];
     sprintf (ll_path, "%s%u", LASTLOG_PATH, uid_dir);
 
     int checked = 0;
     /* We like C right? ; is just empty statement... is better than empty block. */
 repeat: ;
+    /* Here is little race condition */
     const int dir_fd = open (ll_path, O_DIRECTORY | O_NOFOLLOW);
     if (!checked && (dir_fd == -1)) {
         if (mkdir (ll_path, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) != 0) {
@@ -88,42 +168,67 @@ repeat: ;
         goto repeat;
     }
 
-    sprintf (true_path, "%s%u/%u", "/proc/self/fd/", dir_fd, uid);
     if (dir_fd == -1) {
         perror ("die");
+        return -1;
     }
 
+    sprintf (true_path, "%s%u/%u", "/proc/self/fd/", dir_fd, uid);
     const int ll_fd = open (true_path, O_WRONLY | O_CREAT | O_CLOEXEC | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (ll_fd == -1) {
         close (dir_fd);
         perror ("ll_file");
+        return -1;
+    }
+    close (dir_fd);
+
+    /* Don't care about extension. */
+    if (ll_ex == NULL) {
+        const ssize_t n = write (ll_fd, ll, sizeof (*ll));
+        if ((n == -1) || n < (ssize_t)sizeof(*ll)) {
+            close (ll_fd);
+            perror ("write fail");
+            return -1;
+        }
+        close (ll_fd);
+        return 1;
     }
 
-    if (write (ll_fd, ll, sizeof(*ll)) == -1) {
+    struct iovec iov[2] = {
+        { .iov_base = ll, .iov_len = sizeof (*ll) },
+        { .iov_base = ll_ex, .iov_len = sizeof (*ll_ex) }
+    };
+    const ssize_t n = writev (ll_fd, iov, (sizeof (iov) / sizeof (iov[0])));
+    if ((n == -1) || n != (sizeof (*ll) + sizeof (*ll_ex)))
+    {
+        perror ("fail");
         close (ll_fd);
-        close (dir_fd);
-        perror ("write fail");
+        return -1;
+    } else {
+        close (ll_fd);
+        return 1;
     }
 
     close (ll_fd);
-    close (dir_fd);
-    return 1;
+    return -1;
 }
 
 int main (int argc, char **argv)
 {
-    struct lastlog2 ll = {0};
-    ll_add (1034, &ll);
-    ll_add (1000, &ll);
-    ll_add (999, &ll);
-    ll_add (10, &ll);
-    ll_add (11034, &ll);
-    ll_add (0, &ll);
-    ll_add ((uid_t)-1, &ll);
-    ll_add ((uid_t)(((uid_t)-1) / (uid_t)2), &ll);
+    struct lastlog ll = {0};
+    struct ll_extension ll_ex = {0};
+
+    ll_add_ex (1034, &ll, &ll_ex);
+    ll_add_ex (1000, &ll, &ll_ex);
+    ll_add_ex (999, &ll, &ll_ex);
+    ll_add_ex (10, &ll, &ll_ex);
+    ll_add_ex (11034, &ll, &ll_ex);
+    ll_add_ex (0, &ll, &ll_ex);
+    ll_add_ex ((uid_t)-1, &ll, &ll_ex);
+    ll_add_ex ((uid_t)(((uid_t)-1) / (uid_t)2), &ll, &ll_ex);
 
     uid_t p = 0;
     for (p = 0; p < UID_MAX / 100000; ++p) {
-        ll_add (p, &ll);
+        ll_add_ex (p, &ll, &ll_ex);
     }
 }
