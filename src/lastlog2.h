89379928ae1b766739d0698a028d9ba70c21ee3c
Robin Hack <rhack@redhat.com>
2014-05-23 15:21:12 +0200

    Added warn_unused result to function declarations.

--- src/lastlog2.h
+++ src/lastlog2.h
@@ -11,12 +11,11 @@ struct ll_extension {
 
 #define UID_MAX ((uid_t) -1)
 
-/* TODO:  Add attributes. */
 /* Public accessible functions. */
-inline int getlstlogent (const uid_t uid, struct lastlog *const ll);
-inline int getlstlogentx (const uid_t uid, struct lastlog *const ll, struct ll_extension *const ll_ex);
+inline int getlstlogent (const uid_t uid, struct lastlog *const ll) __attribute__ ((warn_unused_result));
+inline int getlstlogentx (const uid_t uid, struct lastlog *const ll, struct ll_extension *const ll_ex) __attribute__ ((warn_unused_result));
 
-inline int putlstlogent (const uid_t uid, const struct lastlog *const ll);
-inline int putlstlogentx (const uid_t uid, const struct lastlog *const ll, const struct ll_extension *const ll_ex);
+inline int putlstlogent (const uid_t uid, const struct lastlog *const ll) __attribute__ ((warn_unused_result));
+inline int putlstlogentx (const uid_t uid, const struct lastlog *const ll, const struct ll_extension *const ll_ex) __attribute__ ((warn_unused_result));
 
 #endif /* _H_LASTLOG2_ */
